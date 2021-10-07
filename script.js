98007c258ce5060dcf51d3729c36c49208cdd9ea
G-Linares <gerrylinares@gmail.com>
2021-10-07 18:58:49 -0500

    Profe Lalo introduce modularidad en una clase orientada o objetos

--- script.js
+++ script.js
@@ -35,3 +35,55 @@ document.querySelector('#push').onclick = function(){
         document.querySelector("#newtask input").value = "";
     }
 }
+
+/*class Manejador {
+    constructor() {
+      this.cajaDeTexto = document.querySelector('#newtask input');
+      this.push = document.querySelector('#push');
+      this.tasks = document.querySelector('#tasks');
+      this.push.onclick = this.escucharEvento.bind(this);
+    }
+    escucharEvento() {
+      if (!this.cajaDeTexto.value) {
+        alert("Porfavor Ingresa una Tarea");
+        return;
+      }
+      const elemento = this.crearElemento(this.cajaDeTexto.value);
+      this.tasks.innerHTML += elemento;
+      this.escucharEventoBorrar();
+      this.escucharEventoTachar();
+      this.limpiar();
+    }
+    crearElemento(valor) {
+      return `
+        <div class="task">
+            <span id="taskname">
+                ${valor}
+            </span>
+            <button class="delete">
+                <i class="far fa-trash-alt"></i>
+            </button>
+        </div>`;
+    }
+    escucharEventoBorrar() {
+      var current_tasks = document.querySelectorAll(".delete");
+      for (var i = 0; i < current_tasks.length; i++) {
+        current_tasks[i].onclick = function () {
+          this.parentNode.remove();
+        }
+      }
+    }
+    escucharEventoTachar() {
+      var tasks = document.querySelectorAll(".task");
+      for (var i = 0; i < tasks.length; i++) {
+        tasks[i].onclick = function () {
+          this.classList.toggle('completed');
+        }
+      }
+    }
+    limpiar() {
+      this.cajaDeTexto.value = "";
+    }
+  }
+
+  */
